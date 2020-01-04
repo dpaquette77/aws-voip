594ff120af78b07fc4ade0eca98620b7c7948609
dpaquette <dpaquette@ubity.com>
2020-01-04 09:56:14 -0500

    add db_url config script to retrive vals from ssm

---
+++ voip-stack/configure_opensips_db_url.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import boto3
+
+CONFIG_FILEPATH = '/usr/local/etc/opensips/opensips.cfg'
+
+DB_HOST_PLACEHOLDER = "DB_HOST"
+DB_NAME_PLACEHOLDER = "DB_NAME"
+DB_USERNAME_PLACEHOLDER = "DB_USERNAME"
+DB_PASSWORD_PLACEHOLDER = "DB_PASSWORD"
+
+ssm = boto3.client('ssm', 'ca-central-1')
+db_host = ssm.get_parameter(Name='opensips-registrar-db-host')
+db_name = ssm.get_parameter(Name='opensips-registrar-db-name')
+db_username = ssm.get_parameter(Name='opensips-registrar-db-username')
+db_password = ssm.get_parameter(Name='opensips-registrar-db-password',
+        WithDecryption=True)
+
+print db_host['Parameter']['Value'], db_name['Parameter']['Value'], db_username['Parameter']['Value'], db_password['Parameter']['Value']
+
+config_file = open(CONFIG_FILEPATH, "rt")
+config_file_text = config_file.read()
+config_file_text_new = config_file_text.replace(DB_HOST_PLACEHOLDER,
+        db_host['Parameter']['Value'])  \
+    .replace(DB_NAME_PLACEHOLDER, db_name['Parameter']['Value']) \
+    .replace(DB_USERNAME_PLACEHOLDER, db_username['Parameter']['Value']) \
+    .replace(DB_PASSWORD_PLACEHOLDER, db_password['Parameter']['Value'])
+config_file.close()
+
+config_file = open(CONFIG_FILEPATH, "wt")
+config_file.write(config_file_text_new)
+config_file.close()
